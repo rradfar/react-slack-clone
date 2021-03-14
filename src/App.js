3f1487ffa50b96df19ed96499b4ea7215b83f26d
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-03-13 19:22:37 -0800

    Fix the left side of the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
 
 function App() {
   return (
